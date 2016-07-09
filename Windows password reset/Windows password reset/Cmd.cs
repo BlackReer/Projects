447ea7e8ea1e924e205e182a39b23d507d20d4e9
BlackReer <BlackReer@DESKTOP-NMUO9JH>
2016-07-09 22:23:00 +0200

    Release !

---
+++ Windows password reset/Windows password reset/Cmd.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Windows_password_reset
+{
+    public class Cmd
+    {
+        public void reset(string value)
+        {
+
+        }
+    }
+}
---
+++ Windows password reset/Windows password reset/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Windows_password_reset
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnPassReset = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(41, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(195, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(41, 63);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(195, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // btnPassReset
+            // 
+            this.btnPassReset.Location = new System.Drawing.Point(41, 91);
+            this.btnPassReset.Name = "btnPassReset";
+            this.btnPassReset.Size = new System.Drawing.Size(195, 23);
+            this.btnPassReset.TabIndex = 2;
+            this.btnPassReset.Text = "Change password";
+            this.btnPassReset.UseVisualStyleBackColor = true;
+            this.btnPassReset.Click += new System.EventHandler(this.btnPassReset_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(198, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Type your new password here";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 131);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnPassReset);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Windows password reset";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnPassReset;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
